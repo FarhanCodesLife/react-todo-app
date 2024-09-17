a06a12d7916cf6591e522e1d65db3de6b022f8bf
MUHAMMADFARHANHANIF <farhansmit0318@gmail.com>
2024-09-18 00:33:30 +0500

    todo firebase

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,45 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './Layout'
+import Todo from './pages/Todo'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import App from './App'
+
+const router = createBrowserRouter([
+  {
+    path:'',
+    element:<Layout />,
+    children:[
+      {path:'/',
+        element:<Todo />
+      },
+      {
+        path:'login',
+        element:<Login />
+
+      },
+      {
+        path:'register',
+        element:<Register />
+      },
+      {
+        path:'*',
+        element:<h1>Not Found</h1>
+      }
+
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  
+<>
+<RouterProvider router={router}>
+<App />
+</RouterProvider>
+
+</>  
+  
+  )
